615b07b09173dc479d877589fafb026a6ca36b06
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-23 12:31:24 -0700

    working on actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,9 @@ export const GET_SINGLE_POST = "GET_SINGLE_POST"
 export const GET_ALL_POSTS = "GET_ALL_POSTS"
 export const DELETE_POST = "DELETE_POST"
 export const CREATE_POST = "CREATE_POST"
+
+export function getBlogPosts(){
+  return async (dispatch) => {
+    const res = 
+  }
+}
