b09f1b8904e3ea2c315f9683883afdde8142afde
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-23 15:03:44 -0700

    funky redux stuff

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,12 @@ export const CREATE_POST = "CREATE_POST"
 
 export function getBlogPosts(){
   return async (dispatch) => {
-    const res = 
+    const res = await request('http://localhost:5000/blog_posts')
+    const json = await res.json()
+
+    dispatch({
+      type: GET_SINGLE_POST,
+      payload: json.blog_posts
+    })
   }
 }
