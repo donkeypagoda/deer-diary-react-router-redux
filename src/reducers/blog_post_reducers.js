f5a90e07b129f1dcad7fea53f2222d826d1b8a24
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-25 15:02:30 -0700

    working on reducers

---
+++ src/reducers/blog_post_reducers.js
@@ -0,0 +1,20 @@
+import {  GET_SINGLE_POST,
+          GET_ALL_POSTS,
+          DELETE_POST,
+          CREATE_POST } from "../actions/index.js"
+
+const getAllPosts = (state, action) => {
+
+}
+
+const getSinglePost = (state, action) => {
+
+}
+
+const deletePost = (state, action) => {
+
+}
+
+const createPost = (state, action) => {
+  
+}
